cb8125998f25d0a9f261752ba31de4ac09d797b7
ManuelPalomares <manuel936@gmail.com>
2014-09-30 21:53:20 -0500

    configuracion menus dinamicos

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -21,7 +21,33 @@ Ext.define('EscuelaFutbol.controller.Main', {
                 accion:"CONSULTARMODULOS"
             },
             success : function(response){
-                var modulos = Ext.decode(responese.responseText);
+                var  varMenu = Ext.decode(response.responseText);
+                //console.log(modulos);
+                for(var i=0; i<varMenu.modulos.length ;i++){
+                    //console.log(varMenu.modulos[i]);
+                    var submenuDatos = varMenu.modulos[i].SUBMENUS;
+                    var submenu = Ext.create('Ext.menu.Menu');
+
+                    //crea menu del objeto submenu
+                    for(var x=0; x<submenuDatos.length;x++){
+                        submenu.add({
+                            text : submenuDatos[x].DESCRIPCION,
+                            xtypeID : submenuDatos[x].WIDGETALIAS
+                        });
+                    }
+                    var boton = Ext.create("Ext.Button",{
+                        xtype : "button",
+                        text  : varMenu.modulos[i].DESCRIPCION,
+                        style : {margin:'0.5em'},
+                        height: 200,
+                        icon : varMenu.modulos[i].URL_ICON,
+                        iconAlign :'top',
+                        iconCls   : 'x_iconosBotones',
+                        menu      : submenu
+                    });
+                    panel.add(boton);
+                }
+               
             },
             failure : function(response){alert('Error realizando su solicitud por favor comunicar con su administrador :' + response.status);}
             
@@ -30,9 +56,8 @@ Ext.define('EscuelaFutbol.controller.Main', {
      },
      llamaOpcion : function(btn,item){
         var itemOpcion = item.text;
-        if(itemOpcion =="Registro de Nuevos clientes"){
-            //alert(":_D");
-            var ventana = Ext.create("Ext.window.Window",{
+        var xtypeID = item.xtypeID;
+        var ventana = Ext.create("Ext.window.Window",{
                 modal: true, 
                 width: 900,
                 height : 600,
@@ -40,262 +65,10 @@ Ext.define('EscuelaFutbol.controller.Main', {
                 title : itemOpcion,
                 maximizable: true,
                 items : [{
-                    xtype : 'nuevosClientes'
+                    xtype : xtypeID
                 }],
                 closable: true
             });
             ventana.show();
         }
-
-        if(itemOpcion =="Envio de informacion de eventos y partidos"){
-            //alert(":_D");
-            var ventana = Ext.create("Ext.window.Window",{
-                modal: true, 
-                width: 900,
-                height : 600,
-                autoScroll:true,
-                title : itemOpcion,
-                maximizable: true,
-                items : [{
-                    xtype : 'envioSolicitudesNuevas'
-                }],
-                closable: true
-            });
-            ventana.show();
-        }
-
-        if(itemOpcion =="Citacion de prueba deportiva"){
-            //alert(":_D");
-            var ventana = Ext.create("Ext.window.Window",{
-                modal: true, 
-                width: 900,
-                height : 600,
-                autoScroll:true,
-                title : itemOpcion,
-                maximizable: true,
-                items : [{
-                    xtype : 'citacionDeportiva'
-                }],
-                closable: true
-            });
-            ventana.show();
-        }
-
-        if(itemOpcion =="Ingreso de pago"){
-            //alert(":_D");
-            var ventana = Ext.create("Ext.window.Window",{
-                modal: true, 
-                width: 900,
-                height : 600,
-                autoScroll:true,
-                title : itemOpcion,
-                maximizable: true,
-                items : [{
-                    xtype : 'recaudo'
-                }],
-                closable: true
-            });
-            ventana.show();
-        }
-
-        if(itemOpcion =="Generacion manual de cobro de pago"){
-            //alert(":_D");
-            var ventana = Ext.create("Ext.window.Window",{
-                modal: true, 
-                width: 900,
-                height : 600,
-                autoScroll:true,
-                title : itemOpcion,
-                maximizable: true,
-                items : [{
-                    xtype : 'clientesPagoPendientes'
-                }],
-                closable: true
-            });
-            ventana.show();
-        }
-
-
-        if(itemOpcion =="Impresion de recibo de pago"){
-            //alert(":_D");
-            var ventana = Ext.create("Ext.window.Window",{
-                modal: true, 
-                width: 900,
-                height : 600,
-                autoScroll:true,
-                title : itemOpcion,
-                maximizable: true,
-                items : [{
-                    xtype : 'impreReciboPag'
-                }],
-                closable: true
-            });
-            ventana.show();
-        }
-
-        if(itemOpcion =="Registro de suscripcion"){
-            //alert(":_D");
-            var ventana = Ext.create("Ext.window.Window",{
-                modal: true, 
-                width: 900,
-                height : 600,
-                autoScroll:true,
-                title : itemOpcion,
-                maximizable: true,
-                items : [{
-                    xtype : 'suscripcion'
-                }],
-                closable: true
-            });
-            ventana.show();
-        }
-
-        if(itemOpcion =="Jugador"){
-            //alert(":_D");
-            var ventana = Ext.create("Ext.window.Window",{
-                modal: true, 
-                width: 900,
-                height : 600,
-                autoScroll:true,
-                title : itemOpcion,
-                maximizable: true,
-                items : [{
-                    xtype : 'jugador'
-                }],
-                closable: true
-            });
-            ventana.show();
-        }
-
-        if(itemOpcion =="Actualizar Datos personales"){
-            //alert(":_D");
-            var ventana = Ext.create("Ext.window.Window",{
-                modal: true, 
-                width: 900,
-                height : 600,
-                autoScroll:true,
-                title : itemOpcion,
-                maximizable: true,
-                items : [{
-                    xtype : 'actualizarDatosJugador'
-                }],
-                closable: true
-            });
-            ventana.show();
-        }
-
-     }
-    /*llamaOpcion : function(vthis,rec){
-        alert(":D");
-        return;
-    	console.log(rec.data.text);
-
-    	//demo 
-    	if(rec.data.text =="Registro de Nuevos clientes"){
-    		//alert(":_D");
-    		var panelPpal = Ext.getCmp("x_principal");
-    		
-    		var tab = panelPpal.add({
-    			title : rec.data.text,
-    			closable: true,
-    			xtype : "nuevosClientes"
-    		});
-    		panelPpal.setActiveTab(tab);
-
-    	}
-
-        if(rec.data.text =="Envio de informacion de eventos y partidos"){
-            //alert(":_D");
-            var panelPpal = Ext.getCmp("x_principal");
-            
-            var tab = panelPpal.add({
-                title : rec.data.text,
-                closable: true,
-                xtype : "envioSolicitudesNuevas"
-            });
-            panelPpal.setActiveTab(tab);
-
-        }
-
-        if(rec.data.text =="Citacion de prueba deportiva"){
-            //alert(":_D");
-            var panelPpal = Ext.getCmp("x_principal");
-            
-            var tab = panelPpal.add({
-                title : rec.data.text,
-                closable: true,
-                xtype : "citacionDeportiva"
-            });
-            panelPpal.setActiveTab(tab);
-
-        }
-
-        
-        if(rec.data.text =="Ingreso de pago"){
-            //alert(":_D");
-            var panelPpal = Ext.getCmp("x_principal");
-            
-            var tab = panelPpal.add({
-                title : rec.data.text,
-                closable: true,
-                xtype : "recaudo"
-            });
-            panelPpal.setActiveTab(tab);
-
-        }
-
-        if(rec.data.text =="Generacion manual de cobro de pago"){
-            //alert(":_D");
-            var panelPpal = Ext.getCmp("x_principal");
-            
-            var tab = panelPpal.add({
-                title : rec.data.text,
-                closable: true,
-                xtype : "clientesPagoPendientes"
-            });
-            panelPpal.setActiveTab(tab);
-
-        }
-
-        if(rec.data.text =="Impresion de recibo de pago"){
-            //alert(":_D");
-            var panelPpal = Ext.getCmp("x_principal");
-            
-            var tab = panelPpal.add({
-                title : rec.data.text,
-                closable: true,
-                xtype : "impreReciboPag"
-            });
-            panelPpal.setActiveTab(tab);
-
-        }
-
-        if(rec.data.text =="Registro de suscripcion"){
-            
-            var panelPpal = Ext.getCmp("x_principal");
-            
-            var tab = panelPpal.add({
-                title : rec.data.text,
-                closable: true,
-                xtype : "suscripcion"
-            });
-            panelPpal.setActiveTab(tab);
-
-        }
-
-       if(rec.data.text =="Jugador"){
-            
-            var panelPpal = Ext.getCmp("x_principal");
-            
-            var tab = panelPpal.add({
-                title : rec.data.text,
-                closable: true,
-                xtype : "jugador"
-            });
-            panelPpal.setActiveTab(tab);
-
-        }
-        
-
-    }*/
 });
