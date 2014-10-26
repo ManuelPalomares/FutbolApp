438ff73271b2a09e4a44383156ff8dd7db332c4e
ManuelPalomares <manuel936@gmail.com>
2014-10-26 13:34:29 -0500

    Envio de mail para la citacion deportiva
    Creacion de script para envio de prueba deportiva

--- app/view/CitacionDeportiva.js
+++ app/view/CitacionDeportiva.js
@@ -196,6 +196,16 @@ Ext.define('EscuelaFutbol.view.CitacionDeportiva', {
                     iconCls: "x_iconosBotonesForma",
                     iconAlign: "center",
                     id: "x_eliminarCita"
+                },{
+                    xtype: "button",
+                    text: "Enviar Mail",
+                    icon: "resources/img/btns/mail.png",
+                    width: 50,
+                    height: 50,
+                    style: {margin: '0.2em'},
+                    iconCls: "x_iconosBotonesForma",
+                    iconAlign: "center",
+                    id: "x_enviarMailCitacionDeportiva"
                 }]
         }]
 
