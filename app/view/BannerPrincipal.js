ce72ec08b0bc17fc6d30b4e6fef59583939015ab
ManuelPalomares <manuel936@gmail.com>
2014-11-08 15:29:25 -0500

    banner principal

--- app/view/BannerPrincipal.js
+++ app/view/BannerPrincipal.js
@@ -12,7 +12,7 @@ Ext.define('EscuelaFutbol.view.BannerPrincipal', {
             xtype: "container",
             width: "60%",
             style: "margin :0.5em;display:inline-block;",
-            html: "<img  alt='logo' width='20%' height='45%' src='resources/img/logoChampions.jpg'></img><div class='x_tituloApp'>Sistema de Gestión Deportiva Escuela de Futbol Champions</div>"
+            html: "<img  alt='logo' width='25%' height='55px' src='resources/img/logoChampions.jpg'></img><div class='x_tituloApp'>Sistema de Gestión Deportiva Escuela de Futbol Champions</div>"
         }, {
             xtype: "container",
             width: "30%",
@@ -26,7 +26,7 @@ Ext.define('EscuelaFutbol.view.BannerPrincipal', {
                     xtype: "container",
                     items: [
                         {xtype: "label", id: "x_labelUsuario", text: "Nombre de usuario : "}, 
-                        {xtype: "label", id: "x_labelNombreUsuario", text: ""}]
+                        {xtype: "label", id: "x_labelNombreUsuario", text: "", style : 'margin-left:0.5em'}]
                 }, {
                     xtype: "button",
                     id: "x_botonCerrarSesion",
